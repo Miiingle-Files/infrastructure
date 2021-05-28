ca75153dd95fe4441d2d10f5ec230838318d4f55
LM Bibera <lyndon.bibera@headhuntr.io>
2021-05-29 01:05:21 +0800

    interface endpoints require a different bit of config, since this is technically just a virtual NIC, we need the private dns to point at this ip address

--- environment/vpc.tf
+++ environment/vpc.tf
@@ -8,9 +8,12 @@ module "vpc" {
   private_subnets = var.vpc_private_cidrs
   public_subnets  = var.vpc_public_cidrs
 
-  single_nat_gateway     = true
-  enable_nat_gateway     = true
+  single_nat_gateway     = false
+  enable_nat_gateway     = false
   one_nat_gateway_per_az = false
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
   tags = local.common_tags
 }
--- environment/vpc_endpoints.tf
+++ environment/vpc_endpoints.tf
@@ -10,3 +10,13 @@ resource "aws_vpc_endpoint_route_table_association" "s3" {
   route_table_id  = module.vpc.private_route_table_ids[count.index]
   vpc_endpoint_id = aws_vpc_endpoint.s3.id
 }
+
+resource "aws_vpc_endpoint" "rds_data" {
+  vpc_id              = module.vpc.vpc_id
+  service_name        = "com.amazonaws.${var.aws_region}.rds-data"
+  vpc_endpoint_type   = "Interface"
+  security_group_ids  = [module.vpc.default_security_group_id]
+  private_dns_enabled = true
+
+  tags = local.common_tags
+}
