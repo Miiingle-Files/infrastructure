4231c463401f73f55da8ab1e96b7159ed459f4ea
LM Bibera <lyndon.bibera@headhuntr.io>
2021-05-30 14:34:51 +0800

    add new repo for the web app

--- ci_pipeline/code_commit.tf
+++ ci_pipeline/code_commit.tf
@@ -2,8 +2,22 @@ resource "aws_codecommit_repository" "platform" {
   repository_name = "${var.reverse_domain}.platform"
 }
 
+resource "aws_codecommit_repository" "app_web" {
+  repository_name = "${var.reverse_domain}.app.web"
+}
+
 resource "aws_codecommit_trigger" "platform" {
   repository_name = aws_codecommit_repository.platform.repository_name
+
+  trigger {
+    name            = "all"
+    events          = ["all"]
+    destination_arn = aws_sns_topic.pipeline_events.arn
+  }
+}
+
+resource "aws_codecommit_trigger" "app_web" {
+  repository_name = aws_codecommit_repository.app_web.repository_name
 
   trigger {
     name            = "all"
